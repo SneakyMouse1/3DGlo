a2d72b80e33fd7ca3e6c7e6362c5930d06010dfe
Simon Smyslov <46712511+SneakyMouse1@users.noreply.github.com>
2022-06-23 12:13:23 +0200

    Lesson20 Modals

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
+<<<<<<< Updated upstream
 import one from './modules/one';
 import two from './modules/two';
 
 one();
 two();
+=======
+import timer from './modules/timer';
+import menu from './modules/menu';
+import modal from "./modules/modal";
+import smoothScroll from './modules/smoothScroll'
+
+timer('24 june 2022');
+menu();
+modal();
+smoothScroll();
+>>>>>>> Stashed changes
