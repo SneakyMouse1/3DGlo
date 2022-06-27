aa642baeba97c7d5b77ced096bf2f6c5c2f75f9b
Simon Smyslov <46712511+SneakyMouse1@users.noreply.github.com>
2022-06-27 19:57:05 +0200

    Update validation.js

--- src/modules/validation.js
+++ src/modules/validation.js
@@ -1,75 +1,38 @@
 const validation = () => {
 
-    const calcSquare = document.querySelector('.calc-square');
-    const calcCount = document.querySelector('.calc-count');
-    const calcDay = document.querySelector('.calc-day');
+    const calcBlockInput= document.querySelectorAll(".calc-block >input");
+    const userName = document.querySelectorAll("input[name = user_name]");
+    const email = document.querySelectorAll("input[name = user_email]");
+    const phone = document.querySelectorAll("input[name = user_phone]");
+  
 
-    const mainInput = document.querySelectorAll('.form-name')[0];
-    const modalInput = document.querySelectorAll('.form-name')[1];
-    const footerInput = document.getElementById('form2-name');
-    const messageInput = document.getElementById('form2-message');
+    calcBlockInput.forEach((item) => {
+      item.addEventListener("input", (e) => {
+        e.target.value = e.target.value.replace(/\D+/, "");
+      });
+    });
+  
 
-    const emailInputs = document.querySelectorAll('.form-email');
+    userName.forEach((name) => {
+      name.addEventListener("input", (e) => {
+        e.target.value = e.target.value.replace(/[^а-я -]/gi, "");
+      });
+    });
+  
 
-    const phoneInputs = document.querySelectorAll('.form-phone');
+    email.forEach((item) => {
+      item.addEventListener("input", (e) => {
+        e.target.value = e.target.value.replace(/[^\w@-_.!~*]/gi, "");
+      });
+    });
+  
 
-    const numberCheck = (input) => {
-        input.addEventListener('blur', (e) => {
-            e.target.value = e.target.value.replace(/\D+/, '');
-        });
-    };
-
-    const textCheck = (input) => {
-        input.addEventListener('blur', (e) => {
-            let word = e.target.value.replace(/[^а-яё\-\s+]/gi, ' ');
-            word = word.replace(/^\s+|\s+$/g, '');
-            word = word.replace(/^\-+|\-+$/g, '');
-            word = word.replace(/\s+/g, ' ');
-            word = word.replace(/\-+/g, '-');
-
-            e.target.value = word;
-            let upWord = word[0].toUpperCase() + word.slice(1);
-            e.target.value = upWord;
-        });
-
-
-    };
-
-    const mailCheck = (input) => {
-        input.forEach((e) => {
-            e.addEventListener('blur', () => {
-                let word = e.value.replace(/[^a-z\@\-\_\.\!\~\*\']/i, '');
-                word = word.replace(/^\-+|\-+$/g, '');
-                word = word.replace(/\-+/g, '-');
-
-                e.value = word;
-            });
-        });
-    };
-
-    const phoneCheck = (input) => {
-        input.forEach((e) => {
-            e.addEventListener('blur', () => {
-                let word = e.value.replace(/[^0-9\-\(\)]/gi, '');
-                word = word.replace(/^\-+|\-+$/g, '');
-                word = word.replace(/\-+/g, '-');
-
-                e.value = word;
-            });
-        });
-    };
-
-    numberCheck(calcSquare);
-    numberCheck(calcCount);
-    numberCheck(calcDay);
-
-    textCheck(mainInput);
-    textCheck(modalInput);
-    textCheck(footerInput);
-
-    mailCheck(emailInputs);
-
-    phoneCheck(phoneInputs);
-};
+    phone.forEach((item) => {
+      item.addEventListener("input", (e) => {
+        e.target.value = e.target.value.replace(/[^()-\d]/g, "");
+      });
+    });
+    
+  };
 
 export default validation;
