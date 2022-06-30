5f731e0b62e24cae6d33e70722d0192a663e9819
Simon Smyslov <46712511+SneakyMouse1@users.noreply.github.com>
2022-06-30 12:33:13 +0200

    Slider added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from './modules/modal';
 import smoothscroll from './modules/smoothscroll';
 import validation from './modules/validation';
 import tabs from './modules/tabs';
+import slider from './modules/slider';
 
 timer("28 june 2022");
 menu();
@@ -11,3 +12,4 @@ modal();
 smoothscroll();
 validation();
 tabs();
+slider();
